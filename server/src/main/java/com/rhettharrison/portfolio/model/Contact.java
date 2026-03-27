ebcb7245707a88bc017cf8af66cb7ebe4a5a3cf2
Rhett Harrison <reharri7@asu.edu>
2026-03-27 14:40:42 -0700

    Replace `@Data` with `@Getter` and `@Setter` in `User` and `Contact` models

--- server/src/main/java/com/rhettharrison/portfolio/model/Contact.java
+++ server/src/main/java/com/rhettharrison/portfolio/model/Contact.java
@@ -1,15 +1,14 @@
 package com.rhettharrison.portfolio.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "contacts")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Contact {
